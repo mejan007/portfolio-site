8081f32905aecc3f28e2d85153dfa0eeaa8669f1
Mejan Lamichhane <84217094+mejan007@users.noreply.github.com>
2025-04-12 13:41:30 +0545

    added favicons

--- src/App.tsx
+++ src/App.tsx
@@ -160,19 +160,11 @@ function App() {
                     enable: true,
                     mode: "push",
                   },
-                  onHover: {
-                    enable: true,
-                    mode: "repulse",
-                  },
                   resize: true,
                 },
                 modes: {
                   push: {
                     quantity: 4,
-                  },
-                  repulse: {
-                    distance: 200,
-                    duration: 0.4,
                   },
                 },
               },
